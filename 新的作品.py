a6c1eff5dd21b536ecd6730d264db9aeb16ad07a
Michael-spec624 <68501737+Michael-spec624@users.noreply.github.com>
2020-07-19 19:57:58 +0800

    Add files via upload

---
+++ 新的作品.py
@@ -0,0 +1,43 @@
+import json
+from flask import Flask
+import gym
+import numpy as np
+env = gym.make('CartPole-v1')
+
+
+def play(env, policy):
+ observation = env.reset()
+ done = False
+ score = 0
+ observations = []
+ for _ in range(5000):
+  observations += [observation.tolist()]
+  if done:
+   break
+  outcome = np.dot(policy, observation)
+  action = 1 if outcome > 0 else 0
+  observation, reward, done, info = env.step(action)
+  score += reward
+ return score, observations
+
+
+max = (0, [], [])
+# 修改接下来两行!
+for _ in range(100):
+ policy = np.random.rand(1, 4) - 0.5
+ score, observations = play(env, policy)
+ if score > max[0]:
+  max = (score, observations, policy)
+print('Max Score', max[0])
+app = Flask(__name__, static_folder='.')
+@app.route("/data")
+def data():
+  return json.dumps(max[1])
+
+
+@app.route('/')
+def root():
+  return app.send_static_file('./index.html')
+
+
+app.run(host='0.0.0.0', port='3000')
---
+++ 演示.py
@@ -0,0 +1,66 @@
+﻿import turtle
+import time
+
+#移动画笔到指定位置
+def Mov(p,x,y):
+    p.up()
+    p.goto(x,y)
+    p.down()
+
+#在指定位置放置盘子
+def MovP(p,x,y,abc):
+    p.clear()
+    Mov(p,x,y)
+    if (abc==1):
+        p.forward(60)
+    if(abc==2):
+        p.forward(40)
+    if(abc==3):
+        p.forward(20)
+    time.sleep(1)
+
+#在画布上移动盘子
+def mov():
+    MovP(pc,90,-95,3)
+    MovP(pb,-20,-95,2)
+    MovP(pc,-10,-85,3)
+    MovP(pa,70,-95,1)
+    MovP(pc,-110,-95,3)
+    MovP(pb,80,-85,2)
+    MovP(pc,90,-75,3)
+
+#绘制汉诺塔游戏背景图
+def bg():
+    Mov(p,-200,-100)
+    p.forward(400)
+    p.setheading(90)
+    for i in range(3):
+        Mov(p,(i-1)*100,-100)
+        p.forward(200)
+        p.write(chr(ord('A')+i), font = ('Arial',20,'normal'))
+    p.hideturtle()
+    MovP(pa,-130,-95,1)
+    MovP(pb,-120,-85,2)
+    MovP(pc,-110,-75,3)
+
+
+
+#设置每只画笔的颜色和尺寸    
+def setpen(p,s,i):
+    p.pensize(s)
+    p.pencolor(pcolor[i])
+
+#主程序
+
+pa=turtle.Pen()
+pb=turtle.Pen()
+pc=turtle.Pen()
+p=turtle.Pen()
+pcolor=['blue','red','orange','green']
+setpen(p,5,0)
+setpen(pa,10,1)
+setpen(pb,10,2)
+setpen(pc,10,3)
+bg()
+mov()
+turtle.done()
